c5a9de6580b4fdd1523837bca92fc4de1d7a873c
Bernardo <programming@bernardomg.com>
2022-10-03 23:42:01 +0200

    Fixed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { MenuLink } from './navigation/model/menu-link';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular-http-basic-auth-example';
+  title = 'angular-jwt-auth-example';
 
   links: MenuLink[] = [{ name: 'login', path: '/login' },{ name: 'data', path: '/data' }];
 
